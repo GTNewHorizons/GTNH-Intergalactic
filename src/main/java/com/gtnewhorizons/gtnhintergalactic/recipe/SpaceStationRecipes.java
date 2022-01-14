50aa89729620f646ad252ead3c6761a518eab0c4
glowredman <35727266+glowredman@users.noreply.github.com>
2022-01-14 18:45:49 +0100

    Add dimensions for Jupiter, Saturn, Uranus and Neptune / SkyProvider rework (#11)
    * Add Dyson Sphere multiblock
    
    * Remove BON2 jar
    
    The GS source is already deobfuscated so it is not neccessary anymore.
    It can always be found at tterrag's maven: https://ci.tterrag.com/job/BON2/
    
    * Update dependencies
    
    * Dyson Sphere -> Dyson Swarm; Reworked structure
    
    * Fixed oversights in "Dyson Sphere -> Dyson Swarm; Reworked structure"
    
    * Add coolant feature
    
    * Add detailed description
    
    * ducktape-solution for resistance not properly displayed by WAWLA
    
    * Change module destruction logic
    
    * Slight change of random numer generation for module destruction
    
    * Update buildscript
    
    * Adapt to GC changes
    
    * Implement computation requirement
    
    * Update buildscript
    
    * Comment out runtimeOnly dependencies
    
    * Update build.gradle
    
    * Remove last file-based dependencies
    
    * Simplify sky providers
    
    * Merge equal WorldChunkManager classes into one
    
    * Add WorldProviders for Jupiter, Neptune, Saturn and Uranus
    
    * Fix java.lang.NullPointerException: Exception ticking world
    
    * Move some core/world classes to core/dimension
    
    * Simplify ChunkProviders for space stations
    
    * Adjust space station costs
    
    * Further cleanup of *Planets classes
    
    * Simplify WorldProvider for space stations
    
    * Remove config options for eris space station
    
    * Fix space station base placement
    
    * Fix placement of mars icon on skybox
    
    * Minor Space Station changes
    
    - Implement `getDayLength()` in `WorldProviderDefaultSpaceStation`
    - Add missing wall block in Space Station structure
    
    * Faster math
    
    * Rework SkyProvider classes to use SkyProviderBase
    
    * Increase spawn height on space stations by 0.1
    
    Sometimes the player spawns inside the block at y=65, which is not intended (the player could fall down if the block at y=64 is missing). By changing the spawn height to y=65.1, the player *should* always spawn ontop of the block at y=65
    
    * Create GSResources.java
    
    * Remove enableErisSS config option
    
    * Remove GL11.glRotatef(360.0f, 1.0f, 0.0f, 0.0f)
    
    * Remove import of WorldProviderJupiterSS
    
    Does not exist yet
    
    Co-authored-by: Martin Robertz <dream-master@gmx.net>

--- src/main/java/com/gtnewhorizons/gtnhintergalactic/recipe/SpaceStationRecipes.java
+++ src/main/java/com/gtnewhorizons/gtnhintergalactic/recipe/SpaceStationRecipes.java
@@ -3,35 +3,39 @@ package galaxyspace.core.recipe;
 import java.util.HashMap;
 
 import galaxyspace.core.config.GSConfigDimensions;
+import galaxyspace.core.register.GSBlocks;
+import gregtech.api.GregTech_API;
 import micdoodle8.mods.galacticraft.api.GalacticraftRegistry;
 import micdoodle8.mods.galacticraft.api.recipe.SpaceStationRecipe;
 import micdoodle8.mods.galacticraft.api.world.SpaceStationType;
 import micdoodle8.mods.galacticraft.planets.mars.ConfigManagerMars;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
 
 public class SpaceStationRecipes {
 
     public static void loadRecipes() {
-        /*if(GSConfigDimensions.enableVenusSS) {
-            HashMap<Object, Integer> inputMap = new HashMap<Object, Integer>();
-            inputMap.put("ingotLead", 64);
-            inputMap.put("ingotTitanium", 16);
-            inputMap.put("waferAdvanced", 2);
-            inputMap.put("ingotDesh", 12);
-            GalacticraftRegistry.registerSpaceStation(new SpaceStationType(GSConfigDimensions.dimensionIDVenusOrbit, GSConfigDimensions.dimensionIDVenus, new SpaceStationRecipe(inputMap)));
-        }
         if(GSConfigDimensions.enableMarsSS) {
             HashMap<Object, Integer> inputMap = new HashMap<Object, Integer>();
-            inputMap.put("ingotCopper", 64);
-            inputMap.put("ingotTitanium", 4);
-            inputMap.put("waferAdvanced", 2);
-            inputMap.put("ingotDesh", 12);
+            inputMap.put(new ItemStack(GSBlocks.MetalsBlock, 1, 9), 230);
+            inputMap.put(new ItemStack(Blocks.glass_pane), 6);
+            inputMap.put("circuitAdvanced", 4);
+            inputMap.put(new ItemStack(GregTech_API.sBlockMachines, 1, 13), 1); //HV Machine Hull
             GalacticraftRegistry.registerSpaceStation(new SpaceStationType(GSConfigDimensions.dimensionIDMarsOrbit, ConfigManagerMars.dimensionIDMars, new SpaceStationRecipe(inputMap)));
         }
-        if(GSConfigDimensions.enableJupiterSS) {
+        if(GSConfigDimensions.enableVenusSS) {
             HashMap<Object, Integer> inputMap = new HashMap<Object, Integer>();
-        	inputMap.put("logWood", 1);
-        	GalacticraftRegistry.registerSpaceStation(new SpaceStationType(GSConfigDimensions.dimensionIDJupiterOrbit, GSConfigDimensions.dimensionIDJupiter, new SpaceStationRecipe(inputMap)));
-        }*/
+            inputMap.put(new ItemStack(GSBlocks.MetalsBlock), 230);
+            inputMap.put(new ItemStack(Blocks.glass_pane), 6);
+            inputMap.put("circuitElite", 4);
+            inputMap.put(new ItemStack(GregTech_API.sBlockMachines, 1, 15), 1); //IV Machine Hull
+            GalacticraftRegistry.registerSpaceStation(new SpaceStationType(GSConfigDimensions.dimensionIDVenusOrbit, GSConfigDimensions.dimensionIDVenus, new SpaceStationRecipe(inputMap)));
+        }
+//        if(GSConfigDimensions.enableJupiterSS) {
+//            HashMap<Object, Integer> inputMap = new HashMap<Object, Integer>();
+//        	inputMap.put("logWood", 1);
+//        	GalacticraftRegistry.registerSpaceStation(new SpaceStationType(GSConfigDimensions.dimensionIDJupiterOrbit, GSConfigDimensions.dimensionIDJupiter, new SpaceStationRecipe(inputMap)));
+//        }
     }
 
 }
