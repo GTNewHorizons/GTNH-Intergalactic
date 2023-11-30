07ce9841ea4e6a81b2258d5205e809542c8eeae7
miozune <miozune@gmail.com>
2023-11-30 17:33:23 +0900

    Cleanup findRecipes for reference

--- src/main/java/com/gtnewhorizons/gtnhintergalactic/recipe/IG_Recipe_Map.java
+++ src/main/java/com/gtnewhorizons/gtnhintergalactic/recipe/IG_Recipe_Map.java
@@ -47,150 +47,83 @@ public class IG_Recipe_Map extends GT_Recipe.GT_Recipe_Map {
             IG_Recipe.IG_SpaceMiningRecipe aRecipe, boolean aNotUnificated, boolean aDontCheckStackSizes, long aVoltage,
             FluidStack[] aFluids, ItemStack aSpecialSlot, int distance, int moduleTier, ItemStack... aInputs) {
         // Search algorithm copied from findRecipe, just fitted to add all found recipes
-        if (this.mRecipeList.isEmpty()) {
-            return null;
-        } else {
-            int var12;
-            if (GregTech_API.sPostloadFinished) {
-                int tAmount;
-                int var13;
-                if (this.mMinimalInputFluids > 0) {
-                    if (aFluids == null) {
-                        return null;
-                    }
 
-                    tAmount = 0;
-                    FluidStack[] var11 = aFluids;
-                    var12 = aFluids.length;
+        if (mRecipeList.isEmpty()) return null;
 
-                    for (var13 = 0; var13 < var12; ++var13) {
-                        FluidStack aFluid = var11[var13];
-                        if (aFluid != null) {
-                            ++tAmount;
-                        }
-                    }
-
-                    if (tAmount < this.mMinimalInputFluids) {
-                        return null;
-                    }
-                }
-
-                if (this.mMinimalInputItems > 0) {
-                    if (aInputs == null) {
-                        return null;
-                    }
-
-                    tAmount = 0;
-                    ItemStack[] var17 = aInputs;
-                    var12 = aInputs.length;
-
-                    for (var13 = 0; var13 < var12; ++var13) {
-                        ItemStack aInput = var17[var13];
-                        if (aInput != null) {
-                            ++tAmount;
-                        }
-                    }
-
-                    if (tAmount < this.mMinimalInputItems) {
-                        return null;
-                    }
-                }
+        if (GregTech_API.sPostloadFinished) {
+            if (mMinimalInputFluids > 0) {
+                if (aFluids == null) return null;
+                int tAmount = 0;
+                for (FluidStack aFluid : aFluids) if (aFluid != null) tAmount++;
+                if (tAmount < mMinimalInputFluids) return null;
             }
-
-            List<IG_Recipe.IG_SpaceMiningRecipe> recipes = new ArrayList<>();
-            if (aNotUnificated) {
-                aInputs = GT_OreDictUnificator.getStackArray(true, (Object[]) aInputs);
+            if (mMinimalInputItems > 0) {
+                if (aInputs == null) return null;
+                int tAmount = 0;
+                for (ItemStack aInput : aInputs) if (aInput != null) ++tAmount;
+                if (tAmount < mMinimalInputItems) return null;
             }
-
-            if (aRecipe != null && !aRecipe.mFakeRecipe
-                    && aRecipe.mCanBeBuffered
-                    && aRecipe.isRecipeInputEqual(false, aDontCheckStackSizes, aFluids, aInputs)) {
-                if (aRecipe.mEnabled && aVoltage * (long) this.mAmperage >= (long) aRecipe.mEUt
-                        && aRecipe.minDistance <= distance
-                        && aRecipe.maxDistance >= distance
-                        && aRecipe.mSpecialValue <= moduleTier) {
-                    recipes.add(aRecipe);
-                }
-            } else {
-                Iterator<GT_Recipe> var15;
-                IG_Recipe.IG_SpaceMiningRecipe tRecipe;
-                int var19;
-                Collection<GT_Recipe> tRecipes;
-                if (this.mUsualInputCount > 0 && aInputs != null) {
-                    ItemStack[] var18 = aInputs;
-                    var19 = aInputs.length;
-
-                    for (var12 = 0; var12 < var19; ++var12) {
-                        ItemStack tStack = var18[var12];
-                        if (tStack != null) {
-                            tRecipes = this.mRecipeItemMap.get(new GT_ItemStack(tStack));
-                            if (tRecipes != null) {
-                                var15 = tRecipes.iterator();
-
-                                while (var15.hasNext()) {
-                                    tRecipe = (IG_Recipe.IG_SpaceMiningRecipe) var15.next();
-                                    if (!tRecipe.mFakeRecipe && tRecipe
-                                            .isRecipeInputEqual(false, aDontCheckStackSizes, aFluids, aInputs)) {
-                                        if (tRecipe.mEnabled && aVoltage * (long) this.mAmperage >= (long) tRecipe.mEUt
-                                                && tRecipe.minDistance <= distance
-                                                && tRecipe.maxDistance >= distance
-                                                && tRecipe.mSpecialValue <= moduleTier) {
-                                            recipes.add(tRecipe);
-                                        }
-                                    }
-                                }
-                            }
-
-                            tRecipes = this.mRecipeItemMap.get(new GT_ItemStack(tStack, true));
-                            if (tRecipes != null) {
-                                var15 = tRecipes.iterator();
-
-                                while (var15.hasNext()) {
-                                    tRecipe = (IG_Recipe.IG_SpaceMiningRecipe) var15.next();
-                                    if (!tRecipe.mFakeRecipe && tRecipe
-                                            .isRecipeInputEqual(false, aDontCheckStackSizes, aFluids, aInputs)) {
-                                        if (tRecipe.mEnabled && aVoltage * (long) this.mAmperage >= (long) tRecipe.mEUt
-                                                && tRecipe.minDistance <= distance
-                                                && tRecipe.maxDistance >= distance
-                                                && tRecipe.mSpecialValue <= moduleTier) {
-                                            recipes.add(tRecipe);
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (this.mMinimalInputItems == 0 && aFluids != null) {
-                    FluidStack[] var20 = aFluids;
-                    var19 = aFluids.length;
-
-                    for (var12 = 0; var12 < var19; ++var12) {
-                        FluidStack aFluid = var20[var12];
-                        if (aFluid != null) {
-                            tRecipes = this.mRecipeFluidMap.get(aFluid.getFluid().getName());
-                            if (tRecipes != null) {
-                                var15 = tRecipes.iterator();
-
-                                while (var15.hasNext()) {
-                                    tRecipe = (IG_Recipe.IG_SpaceMiningRecipe) var15.next();
-                                    if (!tRecipe.mFakeRecipe && tRecipe
-                                            .isRecipeInputEqual(false, aDontCheckStackSizes, aFluids, aInputs)) {
-                                        if (tRecipe.mEnabled && aVoltage * (long) this.mAmperage >= (long) tRecipe.mEUt
-                                                && tRecipe.minDistance <= distance
-                                                && tRecipe.maxDistance >= distance
-                                                && tRecipe.mSpecialValue <= moduleTier) {
-                                            recipes.add(tRecipe);
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            return recipes;
         }
+
+        List<IG_Recipe.IG_SpaceMiningRecipe> recipes = new ArrayList<>();
+        if (aNotUnificated) aInputs = GT_OreDictUnificator.getStackArray(true, (Object[]) aInputs);
+
+        if (aRecipe != null && !aRecipe.mFakeRecipe
+                && aRecipe.mCanBeBuffered
+                && aRecipe.isRecipeInputEqual(false, aDontCheckStackSizes, aFluids, aInputs)) {
+            if (aRecipe.mEnabled && aVoltage * mAmperage >= aRecipe.mEUt
+                    && aRecipe.minDistance <= distance
+                    && aRecipe.maxDistance >= distance
+                    && aRecipe.mSpecialValue <= moduleTier) {
+                recipes.add(aRecipe);
+            }
+        } else {
+            if (mUsualInputCount > 0 && aInputs != null) for (ItemStack tStack : aInputs) if (tStack != null) {
+                Collection<GT_Recipe> tRecipes = mRecipeItemMap.get(new GT_ItemStack(tStack));
+                if (tRecipes != null) for (GT_Recipe recipe : tRecipes) {
+                    IG_Recipe.IG_SpaceMiningRecipe tRecipe = (IG_Recipe.IG_SpaceMiningRecipe) recipe;
+                    if (!tRecipe.mFakeRecipe && tRecipe
+                        .isRecipeInputEqual(false, aDontCheckStackSizes, aFluids, aInputs)) {
+                        if (tRecipe.mEnabled && aVoltage * mAmperage >= tRecipe.mEUt
+                            && tRecipe.minDistance <= distance
+                            && tRecipe.maxDistance >= distance
+                            && tRecipe.mSpecialValue <= moduleTier) {
+                            recipes.add(tRecipe);
+                        }
+                    }
+                }
+
+                tRecipes = mRecipeItemMap.get(new GT_ItemStack(tStack, true));
+                if (tRecipes != null) for (GT_Recipe recipe : tRecipes) {
+                    IG_Recipe.IG_SpaceMiningRecipe tRecipe = (IG_Recipe.IG_SpaceMiningRecipe) recipe;
+                    if (!tRecipe.mFakeRecipe && tRecipe
+                        .isRecipeInputEqual(false, aDontCheckStackSizes, aFluids, aInputs)) {
+                        if (tRecipe.mEnabled && aVoltage * mAmperage >= tRecipe.mEUt
+                            && tRecipe.minDistance <= distance
+                            && tRecipe.maxDistance >= distance
+                            && tRecipe.mSpecialValue <= moduleTier) {
+                            recipes.add(tRecipe);
+                        }
+                    }
+                }
+            }
+
+            if (mMinimalInputItems == 0 && aFluids != null) for (FluidStack aFluid : aFluids) if (aFluid != null) {
+                Collection<GT_Recipe> tRecipes = mRecipeFluidMap.get(aFluid.getFluid().getName());
+                if (tRecipes != null) for (GT_Recipe recipe : tRecipes) {
+                    IG_Recipe.IG_SpaceMiningRecipe tRecipe = (IG_Recipe.IG_SpaceMiningRecipe) recipe;
+                    if (!tRecipe.mFakeRecipe && tRecipe
+                        .isRecipeInputEqual(false, aDontCheckStackSizes, aFluids, aInputs)) {
+                        if (tRecipe.mEnabled && aVoltage * mAmperage >= tRecipe.mEUt
+                            && tRecipe.minDistance <= distance
+                            && tRecipe.maxDistance >= distance
+                            && tRecipe.mSpecialValue <= moduleTier) {
+                            recipes.add(tRecipe);
+                        }
+                    }
+                }
+            }
+        }
+        return recipes;
     }
 }
