502b6d926f4ac2a101ddafcc8c751d4025873b7a
Maya <10861407+serenibyss@users.noreply.github.com>
2025-04-19 14:10:54 -0500

    update blowdryer config and apply latest spotless fixes

--- src/main/java/com/gtnewhorizons/gtnhintergalactic/recipe/SpaceProjectRegistration.java
+++ src/main/java/com/gtnewhorizons/gtnhintergalactic/recipe/SpaceProjectRegistration.java
@@ -32,12 +32,12 @@ import tectech.thing.CustomItemList;
 public class SpaceProjectRegistration implements Runnable {
 
     Fluid solderUEV = FluidRegistry.getFluid("molten.mutatedlivingsolder") != null
-            ? FluidRegistry.getFluid("molten.mutatedlivingsolder")
-            : FluidRegistry.getFluid("molten.solderingalloy");
+        ? FluidRegistry.getFluid("molten.mutatedlivingsolder")
+        : FluidRegistry.getFluid("molten.solderingalloy");
 
     Fluid solderLuV = FluidRegistry.getFluid("molten.indalloy140") != null
-            ? FluidRegistry.getFluid("molten.indalloy140")
-            : FluidRegistry.getFluid("molten.solderingalloy");
+        ? FluidRegistry.getFluid("molten.indalloy140")
+        : FluidRegistry.getFluid("molten.solderingalloy");
 
     /**
      * Register space projects
@@ -53,45 +53,52 @@ public class SpaceProjectRegistration implements Runnable {
      */
     private void registerAsteroidOutpost() {
         ISpaceProject.ISP_Upgrade reinforcedStructure = new SPUpgrade().setUpgradeName("ReinforcedStructure")
-                .setUpgradeUnlocalizedName("ig.sp.upgrade.reinforcedstructure").setUpgradeTotalStages(40)
-                .setUpgradeVoltage(TierEU.RECIPE_UHV).setUpgradeBuildTime(500 * 20)
-                .setUpgradeItemsCost(
-                        getModItem(SuperSolarPanels.ID, "AdminSolarPanel", 1L, 0),
-                        // Neutronium Borosilicate Glass
-                        new ItemStack(ItemRegistry.bw_realglas, 24, 5),
-                        getModItem(OpenComputers.ID, "item", 8, 103),
-                        CustomItemList.Machine_Multi_Computer.get(1),
-                        ItemList.Sensor_UHV.get(1),
-                        ItemList.Emitter_UHV.get(1))
-                .setUpgradeFluidsCost(new FluidStack(solderLuV, 144 * 30));
+            .setUpgradeUnlocalizedName("ig.sp.upgrade.reinforcedstructure")
+            .setUpgradeTotalStages(40)
+            .setUpgradeVoltage(TierEU.RECIPE_UHV)
+            .setUpgradeBuildTime(500 * 20)
+            .setUpgradeItemsCost(
+                getModItem(SuperSolarPanels.ID, "AdminSolarPanel", 1L, 0),
+                // Neutronium Borosilicate Glass
+                new ItemStack(ItemRegistry.bw_realglas, 24, 5),
+                getModItem(OpenComputers.ID, "item", 8, 103),
+                CustomItemList.Machine_Multi_Computer.get(1),
+                ItemList.Sensor_UHV.get(1),
+                ItemList.Emitter_UHV.get(1))
+            .setUpgradeFluidsCost(new FluidStack(solderLuV, 144 * 30));
         ISpaceProject.ISP_Upgrade improvedComputation = new SPUpgrade().setUpgradeName("ImprovedComputation")
-                .setUpgradeUnlocalizedName("ig.sp.upgrade.improvedcomputation").setUpgradeTotalStages(20)
-                .setUpgradeVoltage(TierEU.RECIPE_UEV).setUpgradeBuildTime(750 * 20)
-                .setUpgradeRequirements(new SPRequirements().setUpgrades(reinforcedStructure))
-                .setUpgradeItemsCost(
-                        getModItem(SuperSolarPanels.ID, "PhotonicSolarPanel", 1L, 0),
-                        getModItem(OpenComputers.ID, "item", 32, 103),
-                        // Cosmic Neutronium Borosilicate Glass
-                        new ItemStack(ItemRegistry.bw_realglas, 32, 14),
-                        CustomItemList.Machine_Multi_Computer.get(16),
-                        ItemList.Sensor_UEV.get(1),
-                        ItemList.Emitter_UEV.get(1))
-                .setUpgradeFluidsCost(new FluidStack(solderUEV, 144 * 10));
+            .setUpgradeUnlocalizedName("ig.sp.upgrade.improvedcomputation")
+            .setUpgradeTotalStages(20)
+            .setUpgradeVoltage(TierEU.RECIPE_UEV)
+            .setUpgradeBuildTime(750 * 20)
+            .setUpgradeRequirements(new SPRequirements().setUpgrades(reinforcedStructure))
+            .setUpgradeItemsCost(
+                getModItem(SuperSolarPanels.ID, "PhotonicSolarPanel", 1L, 0),
+                getModItem(OpenComputers.ID, "item", 32, 103),
+                // Cosmic Neutronium Borosilicate Glass
+                new ItemStack(ItemRegistry.bw_realglas, 32, 14),
+                CustomItemList.Machine_Multi_Computer.get(16),
+                ItemList.Sensor_UEV.get(1),
+                ItemList.Emitter_UEV.get(1))
+            .setUpgradeFluidsCost(new FluidStack(solderUEV, 144 * 10));
         ISpaceProject asteroidOutpost = new ProjectAsteroidOutpost().setProjectName("AsteroidOutpost")
-                .setProjectUnlocalizedName("ig.spaceproject.asteroidoutpost")
-                .setProjectUpgrades(reinforcedStructure, improvedComputation)
-                .setProjectRequirements(new SPRequirements().setSpaceBodyType(SolarSystem.KuiperBelt.getType()))
-                .setProjectBuildTime(250 * 20).setProjectStages(40).setProjectVoltage(TierEU.RECIPE_UV)
-                .setProjectTexture(IG_UITextures.PICTURE_SPACE_PROJECT_ASTEROID_OUTPOST).setProjectItemsCost(
-                        // Osmium Borosilicate Glass
-                        new ItemStack(ItemRegistry.bw_realglas, 16, 5),
-                        // Ultra High Strength Concrete
-                        new ItemStack(IGBlocks.DysonSwarmCasing, 32, 9),
-                        ItemList.Machine_LuV_SolarPanel.get(1),
-                        ItemList.Block_NeutroniumPlate.get(16),
-                        ItemList.Sensor_UV.get(1),
-                        ItemList.Emitter_UV.get(1))
-                .setProjectFluidsCost(new FluidStack(solderLuV, 144 * 20));
+            .setProjectUnlocalizedName("ig.spaceproject.asteroidoutpost")
+            .setProjectUpgrades(reinforcedStructure, improvedComputation)
+            .setProjectRequirements(new SPRequirements().setSpaceBodyType(SolarSystem.KuiperBelt.getType()))
+            .setProjectBuildTime(250 * 20)
+            .setProjectStages(40)
+            .setProjectVoltage(TierEU.RECIPE_UV)
+            .setProjectTexture(IG_UITextures.PICTURE_SPACE_PROJECT_ASTEROID_OUTPOST)
+            .setProjectItemsCost(
+                // Osmium Borosilicate Glass
+                new ItemStack(ItemRegistry.bw_realglas, 16, 5),
+                // Ultra High Strength Concrete
+                new ItemStack(IGBlocks.DysonSwarmCasing, 32, 9),
+                ItemList.Machine_LuV_SolarPanel.get(1),
+                ItemList.Block_NeutroniumPlate.get(16),
+                ItemList.Sensor_UV.get(1),
+                ItemList.Emitter_UV.get(1))
+            .setProjectFluidsCost(new FluidStack(solderLuV, 144 * 20));
 
         SpaceProjectManager.addProject(asteroidOutpost);
     }
@@ -101,15 +108,18 @@ public class SpaceProjectRegistration implements Runnable {
      */
     private void registerPlanetScan() {
         ISpaceProject planetScan = new SpaceProject().setProjectName("PlanetScan")
-                .setProjectUnlocalizedName("ig.spaceproject.planetscan").setProjectBuildTime(250 * 20)
-                .setProjectTexture(IG_UITextures.PICTURE_SPACE_PROJECT_PLANETARY_SCAN).setProjectStages(8)
-                .setProjectVoltage(TierEU.RECIPE_UV).setProjectItemsCost(
-                        // Osmium Borosilicate Glass
-                        new ItemStack(ItemRegistry.bw_realglas, 8, 5),
-                        ItemList.Block_NeutroniumPlate.get(4),
-                        ItemList.Sensor_UV.get(2),
-                        ItemList.Emitter_UV.get(2))
-                .setProjectFluidsCost(new FluidStack(solderLuV, 144 * 20));
+            .setProjectUnlocalizedName("ig.spaceproject.planetscan")
+            .setProjectBuildTime(250 * 20)
+            .setProjectTexture(IG_UITextures.PICTURE_SPACE_PROJECT_PLANETARY_SCAN)
+            .setProjectStages(8)
+            .setProjectVoltage(TierEU.RECIPE_UV)
+            .setProjectItemsCost(
+                // Osmium Borosilicate Glass
+                new ItemStack(ItemRegistry.bw_realglas, 8, 5),
+                ItemList.Block_NeutroniumPlate.get(4),
+                ItemList.Sensor_UV.get(2),
+                ItemList.Emitter_UV.get(2))
+            .setProjectFluidsCost(new FluidStack(solderLuV, 144 * 20));
 
         SpaceProjectManager.addProject(planetScan);
     }
